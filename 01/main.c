2b520a6b18bf653833c98f6b98f65c269d2693e4
Sebastian Seitz <sebastian.seitz1@hs-augsburg.de>
2022-12-02 21:52:16 +0000

    started day one, file IO

---
+++ 01/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+    FILE *file;
+
+    if ((file = fopen("input.txt","r")) == NULL){
+        printf("Error! openinng File");
+        exit(1);
+    }
+    
+    char line[10];
+    int values[3000];
+    int value = atoi(line);
+    int value_idx = 0;
+
+    while(*file!=EOF){
+        fscanf(file,"%[^\n]",line);
+        value_idx++;
+        values[value_idx] = atoi(line);
+
+        printf("Current line: %d\n",values[value_idx]);
+    }
+        
+    fclose(file);
+    return 0;
+}
