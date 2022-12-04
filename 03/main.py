9b052912495abb507692be96ba9a5f7bee50f512
Sebastian Seitz <sebastian.seitz1@hs-augsburg.de>
2022-12-04 17:59:03 +0100

    day 3 done python§

--- 03/main.py
+++ 03/main.py
@@ -1,10 +1,10 @@
 def main():
     with open("input.txt","r") as f:
         inp = f.read().split("\n")
-    
+    sol = 0
     for line in inp:
-        find_duplicate(line)
-    
+        sol+= find_duplicate(line)
+    print(sol)
     
 def find_duplicate(line):
     firstHalf = line[:len(line)//2]
@@ -12,10 +12,8 @@ def find_duplicate(line):
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" 
     for char in firstHalf:
         if char in set(secondHalf):
-            print(char)
+            print(char, letters.index(char))
             return letters.index(char)+1
-           
-def prioritize_duplicate(char):
-    pass
+
 if __name__ == "__main__":
     main()
