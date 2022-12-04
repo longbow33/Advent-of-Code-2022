d92f50ab87078fd172fb8c901d805da381d88401
Sebastian Seitz <sebastian.seitz1@hs-augsburg.de>
2022-12-04 18:43:44 +0100

    day3 done, just in python3

--- 03/main.py
+++ 03/main.py
@@ -5,7 +5,19 @@ def main():
     for line in inp:
         sol+= find_duplicate(line)
     print(sol)
-    
+# PART2
+    # group lines in 3
+    sol2 = 0
+    for i in range(len(inp)//3):
+        elves = inp[i*3:i*3+3]
+        #print(elves)
+        for char in elves[0]:
+            if char in elves[1] and char in elves[2]:
+                badge = char    
+        letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+        sol2+= letters.index(badge)+1
+    print(sol2)
+
 def find_duplicate(line):
     firstHalf = line[:len(line)//2]
     secondHalf = line[len(line)//2:]
